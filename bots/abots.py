558228c394a414bc3523465d6d2f4e98e7dad86d
Anders Birk Sørensen <anders.b.sorensen@ufst.dk>
2021-11-05 17:24:29 +0100

    Best bot!

--- bots/abots.py
+++ bots/abots.py
@@ -1,10 +1,10 @@
 """ABIS player"""
-import random
 from typing import Sequence
 
 from bots.BotInterface import BotInterface
-from environment.Constants import Action
+from environment.Constants import Action, Stage
 from environment.Observation import Observation
+from utils.handValue import getHandPercent
 
 
 # your bot class, rename to match the file name
@@ -16,16 +16,34 @@ class Abots(BotInterface):
         super().__init__(name=name)
 
     def act(self, action_space: Sequence[Action], observation: Observation) -> Action:
-        '''
-            This function gets called whenever it's your bots turn to act.
-                Parameters:
-                    action_space (Sequence[Action]): list of actions you are allowed to take at the current state. 
-                    observation (Observation): all information available to your bot at the current state. See environment/Observation for details
-                returns:
-                    action (Action): the action you want you bot to take. Possible actions are: FOLD, CHECK, CALL and RAISE
-            If this function takes longer than 1 second, your bot will fold
-        '''
+        # use different strategy depending on pre or post flop (before or after community cards are delt)
+        stage = observation.stage
+        if stage == Stage.PREFLOP:
+            return self.handlePreFlop(observation)
 
-        # do a random action
-        action = random.choice(action_space)
-        return action
+        return self.handlePostFlop(observation)
+
+    def handlePreFlop(self, observation: Observation) -> Action:
+        # get my hand's percent value (how good is this 2 card hand out of all possible 2 card hands)
+        handPercent, _ = getHandPercent(observation.myHand)
+        # if my hand is top 20 percent: raise
+        if handPercent < .20:
+            return Action.RAISE
+        # if my hand is top 60 percent: call
+        elif handPercent < .60:
+            return Action.CALL
+        # else fold
+        return Action.FOLD
+
+    def handlePostFlop(self, observation: Observation) -> Action:
+        # get my hand's percent value (how good is the best 5 card hand i can make out of all possible 5 card hands)
+        handPercent, cards = getHandPercent(
+            observation.myHand, observation.boardCards)
+        # if my hand is top 30 percent: raise
+        if handPercent <= .30:
+            return Action.RAISE
+        # if my hand is top 80 percent: call
+        elif handPercent <= .80:
+            return Action.CALL
+        # else fold
+        return Action.FOLD
